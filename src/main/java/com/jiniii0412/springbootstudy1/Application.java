90a9867397b61e860c4d764b2b0d8967800a8723
jinii0412 <jiniii0412@gmail.com>
2020-12-10 22:04:25 +0900

    test1

---
+++ src/main/java/com/jiniii0412/springbootstudy1/Application.java
@@ -0,0 +1,13 @@
+package com.jiniii0412.springbootstudy1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jiniii0412/springbootstudy1/Springbootstudy1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiniii0412.springbootstudy1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootstudy1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
